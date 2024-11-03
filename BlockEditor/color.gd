ab017c06997034efa92978c88d76d136e95515b9
zHoeshin <Ayron64632723837@gmail.com>
2024-11-03 16:27:35 +0300

    Color and reverse block bugfix

--- BlockEditor/color.gd
+++ BlockEditor/color.gd
@@ -3,4 +3,4 @@ extends ColorPickerButton
 func onChange(newcolor: Color):
 	color.r8 = round(newcolor.r8 / 17.) * 17.
 	color.g8 = round(newcolor.g8 / 17.) * 17.
-	color.b8 = round(newcolor.b8 / 1.7) * 17.
+	color.b8 = round(newcolor.b8 / 17.) * 17.
